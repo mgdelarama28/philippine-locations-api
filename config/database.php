da1039e40b4e83e3a2ab6e6d2b9767515071040f
Michael Geofrey Dela Rama <mgdelarama.28@gmail.com>
2020-10-20 15:00:28 +0800

    Configured database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://srtsalolwewfpq:1c417ab42bb9b2b025762c3d84bca87cbb9992263ce00c5f4a7ca132252b2230@ec2-54-224-175-142.compute-1.amazonaws.com:5432/d3tgaap3frnkc1');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $DATABASE_URL['host']),
+            'port' => env('DB_PORT', $DATABASE_URL['port']),
+            'database' => env('DB_DATABASE', ltrim($DATABASE_URL['path'], "/")),
+            'username' => env('DB_USERNAME', $DATABASE_URL['user']),
+            'password' => env('DB_PASSWORD', $DATABASE_URL['pass']),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
